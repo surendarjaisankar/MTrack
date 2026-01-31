26a8be39710a5393909f4633f677201b75f0e971
Surendar Jaisankar <125639407+surendarjaisankar@users.noreply.github.com>
2026-02-01 01:37:58 +0530

    Delete backend directory

--- backend/models/settings.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const settingsSchema = new mongoose.Schema({
-  baseFare: { type: Number, default: 20 },
-  perKmRate: { type: Number, default: 15 },
-  driverCommission: { type: Number, default: 70 } // %
-});
-
-module.exports = mongoose.model("Settings", settingsSchema);
--- backend/routes/feedbackRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/feedbackController");
-
-router.post("/:bookingId", controller.submitFeedback);
-
-module.exports = router;
